bf5a4f8d7c14112cc8c7d44ed0083c064d9c1cb1
Cameron Greene <125625082+Cgreen9639-ux@users.noreply.github.com>
2023-05-10 21:02:28 +0000

    Pending changes exported from your codespace

---
+++ app.js
@@ -0,0 +1,17 @@
+const colors = {green, red, “rgba(133,122, 200)”,”#f15025};
+const btn = document.getElementById(“btn”)
+const color = documentqueryselector(“.color”)
+
+btn.addEventListener(‘Click, function{} {
+const randomnumber = 2
+//get random number between 0-3
+}
+document.body.style.backgroundColor = colors{randomColors};
+colorContext = colors{randomNumber};
+});
+
+function getrandomNumber() {
+Return Math.floor(Math.random() * color.length;
+
+}
+
